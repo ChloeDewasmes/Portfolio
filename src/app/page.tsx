ea7d2aacf121e2f4a3e1995dce8924abf03a1a5f
ChloeDewasmes <155570517+ChloeDewasmes@users.noreply.github.com>
2025-04-16 13:54:47 +0200

    aurora background

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,6 +11,10 @@ import ScrollToTopButton from "./components/scrollToTopButton";
 import { useLang } from "./LangContext";
 import translations from "./translations";
 
+import { motion } from "motion/react";
+import React from "react";
+import { AuroraBackground } from "./components/aurora-background";
+
 export default function Home() {
   const { lang } = useLang();
   const text = translations[lang].page;
@@ -20,72 +24,85 @@ export default function Home() {
         <Header />
       </header>
 
-      <div className="flex flex-col items-center justify-center [@media(min-aspect-ratio:1/1)]:flex-row-reverse h-[90vh]">
-        {/* Contenu principal */}
-        <div className="flex justify-center py-6 [@media(min-aspect-ratio:1/1)]:w-1/2">
-          <Image
-            className="object-contain min-h-[250px] max-w-[80%] max-h-[80vh] [@media(max-aspect-ratio:1/1)]:max-h-[50vh]"
-            src={Profile}
-            alt="Profile picture"
-            priority
-          />
-        </div>
+      <AuroraBackground className="-mt-[60px]">
+        <motion.div
+          initial={{ opacity: 0.0, y: 40 }}
+          whileInView={{ opacity: 1, y: 0 }}
+          transition={{
+            delay: 0.3,
+            duration: 0.8,
+            ease: "easeInOut",
+          }}
+          className="relative flex flex-col gap-4 items-center justify-center"
+        >
+          <div className="flex flex-col items-center justify-center [@media(min-aspect-ratio:1/1)]:flex-row-reverse h-[90vh]">
+            {/* Contenu principal */}
+            <div className="flex justify-center py-6 [@media(min-aspect-ratio:1/1)]:w-1/2">
+              <Image
+                className="object-contain min-h-[250px] max-w-[80%] max-h-[80vh] [@media(max-aspect-ratio:1/1)]:max-h-[50vh]"
+                src={Profile}
+                alt="Profile picture"
+                priority
+              />
+            </div>
 
-        {/* Titre et liens */}
-        <div className="text-light-grey text-center w-full [@media(min-aspect-ratio:1/1)]:w-1/2">
-          <div className="w-full space-y-4 pb-clamp text-clamp [@media(min-aspect-ratio:1/1)]:text-left">
-            <p>{text.hero.greeting}</p>
-            <h1 className="leading-none title-text-clamp font-extralight bg-gradient-to-br from-light-grey via-light-grey to-black bg-clip-text text-transparent text-wrap">
-              {text.hero.job}
-            </h1>
-            <p>{text.hero.location}</p>
-          </div>
+            {/* Titre et liens */}
+            <div className="text-light-grey text-center w-full [@media(min-aspect-ratio:1/1)]:w-1/2">
+              <div className="w-full space-y-4 pb-clamp text-clamp [@media(min-aspect-ratio:1/1)]:text-left">
+                <p>{text.hero.greeting}</p>
+                <h1 className="leading-none title-text-clamp font-extralight bg-gradient-to-br from-light-grey via-light-grey to-black bg-clip-text text-transparent text-wrap">
+                  {text.hero.job}
+                </h1>
+                <p>{text.hero.location}</p>
+              </div>
 
-          <div className="flex flex-row portrait-tall:flex-col items-center gap-6 [@media(max-aspect-ratio:1/1)]:justify-center">
-            <a
-              href="https://drive.google.com/file/d/1dPVVsRLzPHbnTCyWSt5hFijbl-e6YjaV/view?usp=sharing"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <Button text={text.hero.resumeButton} />
-            </a>
-            <div className="flex gap-4">
-              <a
-                href="https://www.linkedin.com/in/chloe-dewasmes/"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                <div className="relative group">
-                  <Linkedin
-                    className="text-light-grey group-hover:opacity-0 icon-responsive"
-                    style={{ strokeWidth: 0.5 }}
-                  />
-                  <Linkedin
-                    className="text-light-grey absolute top-0 left-0 opacity-0 group-hover:opacity-100 icon-responsive"
-                    style={{ strokeWidth: 1.2 }}
-                  />
+              <div className="flex flex-row portrait-tall:flex-col items-center gap-6 [@media(max-aspect-ratio:1/1)]:justify-center">
+                <a
+                  href="https://drive.google.com/file/d/1dPVVsRLzPHbnTCyWSt5hFijbl-e6YjaV/view?usp=sharing"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <Button text={text.hero.resumeButton} />
+                </a>
+                <div className="flex gap-4">
+                  <a
+                    href="https://www.linkedin.com/in/chloe-dewasmes/"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    <div className="relative group">
+                      <Linkedin
+                        className="text-light-grey group-hover:opacity-0 icon-responsive"
+                        style={{ strokeWidth: 0.5 }}
+                      />
+                      <Linkedin
+                        className="text-light-grey absolute top-0 left-0 opacity-0 group-hover:opacity-100 icon-responsive"
+                        style={{ strokeWidth: 1.2 }}
+                      />
+                    </div>
+                  </a>
+                  <a
+                    href="https://github.com/ChloeDewasmes"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    <div className="relative group">
+                      <Github
+                        className="text-light-grey group-hover:opacity-0 icon-responsive"
+                        style={{ strokeWidth: 0.5 }}
+                      />
+                      <Github
+                        className="text-light-grey absolute top-0 left-0 opacity-0 group-hover:opacity-100 icon-responsive"
+                        style={{ strokeWidth: 1.2 }}
+                      />
+                    </div>
+                  </a>
                 </div>
-              </a>
-              <a
-                href="https://github.com/ChloeDewasmes"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                <div className="relative group">
-                  <Github
-                    className="text-light-grey group-hover:opacity-0 icon-responsive"
-                    style={{ strokeWidth: 0.5 }}
-                  />
-                  <Github
-                    className="text-light-grey absolute top-0 left-0 opacity-0 group-hover:opacity-100 icon-responsive"
-                    style={{ strokeWidth: 1.2 }}
-                  />
-                </div>
-              </a>
+              </div>
             </div>
           </div>
-        </div>
-      </div>
+        </motion.div>
+      </AuroraBackground>
 
       {/* Sections */}
       <div className="min-h-screen py-20" id="about">
